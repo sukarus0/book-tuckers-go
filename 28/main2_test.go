28522da2a33c32f95e3a23c439e03af51d7a9cf6
sukarus0 <sukarus@gamil.com>
2022-10-13 15:10:45 +0000

    DONE 28

---
+++ 28/main2_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify"
+)
+
+func TestSquare1(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(81, square(9), "square(9) should be 81")
+}
+
+func TestSquare2(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(9, square(3), "square(3) should be 9")
+}
