24217f61cdf22948924cc1c451a8350a51e493d4
sukarus0 <sukarus@gamil.com>
2022-10-14 00:57:42 +0000

    FIX code errors

--- 28/main2_test.go
+++ 28/main2_test.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"testing"
-	"github.com/stretchr/testify"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSquare1(t *testing.T) {
+func TestSquare3(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal(81, square(9), "square(9) should be 81")
 }
 
-func TestSquare2(t *testing.T) {
+func TestSquare4(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal(9, square(3), "square(3) should be 9")
 }
